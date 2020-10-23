0420de0483d22755369b405964430b0605520a41
unknown <Administrador@n087030.int.cagece.com.br>
2020-10-22 22:13:03 -0300

    Primeiro commit enviando os arquivos para o git

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.col-sm-4').magnificPopup({
+        delegate: 'a', // seletor de itens filho, clicando nele, o pop-up será aberto
+        type: 'image',
+        gallery:{enabled:true}
+        // outras opções
+      });
+  });
